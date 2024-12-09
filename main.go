f18362df026ba91388fad0ed2483e208c9749a74
zhangweisong <zhangweisong@mixlinker.com>
2024-12-09 19:10:37 +0800

    完成插入排序

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 func test() {
 	arr := utils.RandIntArr(0, 100, 10)
-	sort.SelectSort(arr)
+	sort.InsertSort(arr)
 	utils.VarDump(arr)
 	fmt.Println(utils.Check(arr))
 }
--- sort/insert.go
+++ sort/insert.go
@@ -1,6 +1,14 @@
 package sort
 
 func InsertSort(arr []int) {
-	// 选择最小的，放在第i个
-	
+	n := len(arr)
+	for i := 1; i <= n-1; i++ {
+		for j := i - 1; j >= 0; j-- {
+			if arr[j+1] < arr[j] {
+				_swap(arr, j, j+1)
+			} else {
+				break
+			}
+		}
+	}
 }
