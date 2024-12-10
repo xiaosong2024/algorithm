2cda5483a96d2674bf9ff92a234d8965c632beff
zhangweisong <zhangweisong@mixlinker.com>
2024-12-10 17:48:30 +0800

    添加二叉树算法

--- main.go
+++ main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"algorithm/sort"
+	"algorithm/tree"
 	"algorithm/utils"
 	"fmt"
 )
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		test()
+
+	tree1 := &tree.BiTree{
+		Value: 1,
+		LeftChild: &tree.BiTree{
+			Value: 2,
+			LeftChild: &tree.BiTree{
+				Value:      4,
+				LeftChild:  nil,
+				RightChild: nil,
+			},
+			RightChild: &tree.BiTree{
+				Value:      5,
+				LeftChild:  nil,
+				RightChild: nil,
+			},
+		},
+		RightChild: &tree.BiTree{
+			Value: 3,
+			LeftChild: &tree.BiTree{
+				Value:      6,
+				LeftChild:  nil,
+				RightChild: nil,
+			},
+			RightChild: &tree.BiTree{
+				Value:      7,
+				LeftChild:  nil,
+				RightChild: nil,
+			},
+		},
 	}
+	tree.Floor(tree1)
 }
 
 func test() {
