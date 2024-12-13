c87f2a401ed51ede05d0ab6da55689747f02ae5d
zhangweisong <zhangweisong@mixlinker.com>
2024-12-13 18:30:53 +0800

    add

--- main.go
+++ main.go
@@ -1,66 +1,21 @@
 package main
 
 import (
-	"algorithm/sort"
-	"algorithm/utils"
-	"time"
+	"fmt"
 )
 
 func main() {
-	test(sort.QuickSort)
+	arr := []int{1, 2, 3, 4, 5, 6}
+	backPrint(arr, 0)
 }
 
-func timeTest() {
-	go func() {
-		t := time.Now()
-		for i := 0; i < 1000; i++ {
-			test(sort.BubbleSort)
-		}
-		utils.VarDump("BubbleSort", time.Now().Sub(t).Seconds())
-	}()
-
-	go func() {
-		t := time.Now()
-		for i := 0; i < 1000; i++ {
-			test(sort.SelectSort)
-		}
-		utils.VarDump("SelectSort", time.Now().Sub(t).Seconds())
-	}()
-
-	go func() {
-		t := time.Now()
-		for i := 0; i < 1000; i++ {
-			test(sort.InsertSort)
-		}
-		utils.VarDump("InsertSort", time.Now().Sub(t).Seconds())
-	}()
-
-	go func() {
-		t := time.Now()
-		for i := 0; i < 1000; i++ {
-			test(sort.MergeSort)
-		}
-		utils.VarDump("MergeSort", time.Now().Sub(t).Seconds())
-	}()
-
-	go func() {
-		t := time.Now()
-		for i := 0; i < 1000; i++ {
-			test(sort.QuickSort)
-		}
-		utils.VarDump("QuickSort", time.Now().Sub(t).Seconds())
-	}()
-	for {
+func backPrint(arr []int, cur int) {
+	n := len(arr)
+	left := n - cur
+	if left == 0 {
+		return
 	}
-
-}
-
-func test(f func([]int)) {
-	arr := utils.RandIntArr(0, 10, 10)
-	utils.VarDump(arr)
-	f(arr)
-	utils.VarDump(arr)
-	if err := utils.Check(arr); err != nil {
-		utils.VarDump(err)
-	}
+	next := cur + 1
+	backPrint(arr, next)
+	fmt.Println(arr[cur])
 }
